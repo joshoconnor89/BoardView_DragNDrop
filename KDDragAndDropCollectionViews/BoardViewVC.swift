2f50292be8e0e13173904956da322c9f461fe244
Josh O'Connor <josh.0@joshs-workstation.local>
2017-02-13 12:18:35 -0800

    Set up dummy data for prototype

--- KDDragAndDropCollectionViews/BoardViewVC.swift
+++ KDDragAndDropCollectionViews/BoardViewVC.swift
@@ -13,6 +13,16 @@ class BoardViewVC: UIViewController {
     
     @IBOutlet weak var boardViewCollectionView: UICollectionView!
     
+    var firstListData = ["Red", "Blue", "Green", "Brown", "Black", "Purple", "Orange", "Gray", "White", "Yellow", "Teal", "Magenta"]
+    var secondListData = ["Korean War", "WWI", "WWII", "Mexican Revolution", "Brooks-Baxter War", "Greek Punic Wars", "First Crusade", "Russian Revolution", "Korean War", "Vietnam War", "Gulf War"]
+    var thirdListData = ["Captain Crunch", "Reeses Puff", "Fruit Loops", "Fruity Pebbles", "Cocoa Puffs", "Raisin Bran", "Honey Nut Cheerios", "Apple Jacks", "Cinnamon Toast Crunch"]
+    var fourthListData = ["Oakland Raiders", "New England Patriots", "Carolina Panthers", "Green Bay Packers", "San Francisco 49ers", "San Diego Chargers", "Denver Broncos", "Detroit Lions", "Seattle Seahawks", "Minnesota Vikings", "Atlanta Falcons"]
+    var fifthListData = ["United States of America", "Canada", "Mexico", "England", "Germany", "Japan", "Korea", "China", "India", "Russia", "Israel", "Colombia", "Norway", "Poland", "Spain"]
+    var sixthListData = ["Cat", "Dog", "Owl", "Manatee", "Gorilla", "Snake", "Goat", "Cow", "Chicken", "Pig", "Ostrich", "Alligator", "Elephant", "Bear", "Salmon", "Platypus", "Chameleon"]
+    var seventhListData = ["Soronan Desert", "Kalahari Desert", "Gobi Desert", "Mojave Desert", "Great Basin Desert", "Thar Desert", "Great Sandy Desert", "Gibson Desert", "Namib Desert"]
+    var eighthListData = ["Pacific Ocean", "Atlantic Ocean", "Indian Ocean", "Southern Ocean", "Artic Ocean"]
+    var ninthListData = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Pluto"]
+    var tenthListData = ["Helium", "Argon", "Krypton", "Iron", "Gold", "Mercury", "Uranium", "Lead", "Bromine", "Iodine", "Lithium", "Magnesium", "Hydrogen", "Carbon", "Calcium", "Nickel", "Cobalt", "Phosphorus", "Sulfur", "Oxygen", "Nitrogen", "Sodium"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -51,24 +61,94 @@ extension BoardViewVC: UICollectionViewDataSource {
     
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 20
+        if (collectionView == boardViewCollectionView){
+            return 10
+        }else{
+            //Because we are not using Core Data, this will be verbose due to local arrays
+            if let tag = (collectionView.superview?.superview as? ListCell)?.tag {
+                switch tag {
+                case 0:
+                    return firstListData.count
+                case 1:
+                    return secondListData.count
+                case 2:
+                    return thirdListData.count
+                case 3:
+                    return fourthListData.count
+                case 4:
+                    return fifthListData.count
+                case 5:
+                    return sixthListData.count
+                case 6:
+                    return seventhListData.count
+                case 7:
+                    return eighthListData.count
+                case 8:
+                    return ninthListData.count
+                case 9:
+                    return tenthListData.count
+                default:
+                    break
+                }
+            }
+
+            
+            return 5
+        }
+        
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
         
-        if collectionView == boardViewCollectionView {
+        if (collectionView == boardViewCollectionView) {
             if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "listCell", for: indexPath) as? ListCell{
                 let cellNib = UINib(nibName: String(describing: ListItemCell.self), bundle: nil)
                 cell.listCollectionView.register(cellNib, forCellWithReuseIdentifier: "listItemCell")
                 
                 cell.listCollectionView.delegate = self
                 cell.listCollectionView.dataSource = self
+                cell.tag = indexPath.row
                 
                 return cell
             }
         }else{
             if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "listItemCell", for: indexPath) as? ListItemCell{
+                
+                if let parentCV = collectionView.superview?.superview as? ListCell {
+                    let tag = parentCV.tag
+                    
+                    var string = ""
+                    switch tag {
+                    case 0:
+                        string = firstListData[indexPath.row]
+                    case 1:
+                        string = secondListData[indexPath.row]
+                    case 2:
+                        string = thirdListData[indexPath.row]
+                    case 3:
+                        string = fourthListData[indexPath.row]
+                    case 4:
+                        string = fifthListData[indexPath.row]
+                    case 5:
+                        string = sixthListData[indexPath.row]
+                    case 6:
+                        string = seventhListData[indexPath.row]
+                    case 7:
+                        string = eighthListData[indexPath.row]
+                    case 8:
+                        string = ninthListData[indexPath.row]
+                    case 9:
+                        string = tenthListData[indexPath.row]
+                    default:
+                        break
+                    }
+                    
+                    cell.listItemLabel.text = string
+                    
+                    
+                }
+                
                 
                 return cell
             }
@@ -76,5 +156,15 @@ extension BoardViewVC: UICollectionViewDataSource {
        
         return UICollectionViewCell()
         
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        if (collectionView == boardViewCollectionView){
+            if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "listCell", for: indexPath) as? ListCell{
+
+                cell.listCollectionView.reloadData()
+                
+            }
+        }
     }
 }
--- KDDragAndDropCollectionViews/ListItemCell.swift
+++ KDDragAndDropCollectionViews/ListItemCell.swift
@@ -11,4 +11,5 @@ import UIKit
 
 class ListItemCell: UICollectionViewCell {
 
+    @IBOutlet weak var listItemLabel: UILabel!
 }
