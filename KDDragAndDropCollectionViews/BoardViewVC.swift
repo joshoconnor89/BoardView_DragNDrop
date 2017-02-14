b4f2e805809a2a00fb9eb79fcd76077516228ea3
Josh O'Connor <josh.0@joshs-workstation.local>
2017-02-13 16:32:31 -0800

    Get indexPath of both tableviews

--- KDDragAndDropCollectionViews/BoardViewVC.swift
+++ KDDragAndDropCollectionViews/BoardViewVC.swift
@@ -50,7 +50,17 @@ class BoardViewVC: UIViewController {
         let locationInView = longPress.location(in: boardViewCollectionView)
 
         let indexPath = self.boardViewCollectionView.indexPathForItem(at: locationInView)
-        print(indexPath)
+        
+        print("main cell indexpath \(indexPath)")
+        
+        
+        
+        if let cellSelected = self.boardViewCollectionView.cellForItem(at: indexPath!) as? ListCell {
+            let locationInCellTableView = longPress.location(in: cellSelected)
+            let indexPath = cellSelected.listCollectionView.indexPathForItem(at: locationInView)
+            
+            print("cell in main cell indexpath \(indexPath)")
+        }
         
         struct CellBeingMoved {
             static var cellSnapshot : UIView? = nil
@@ -266,7 +276,20 @@ class BoardViewVC: UIViewController {
         }
     }
     
-
+    func snapshotOfCell(_ inputView: UIView) -> UIView {
+        UIGraphicsBeginImageContextWithOptions(inputView.bounds.size, false, 0.0)
+        inputView.layer.render(in: UIGraphicsGetCurrentContext()!)
+        let image = UIGraphicsGetImageFromCurrentImageContext()! as UIImage
+        UIGraphicsEndImageContext()
+        
+        let cellSnapshot : UIView = UIImageView(image: image)
+        cellSnapshot.layer.masksToBounds = false
+        cellSnapshot.layer.cornerRadius = 0.0
+        cellSnapshot.layer.shadowOffset = CGSize(width: -5.0, height: 0.0)
+        cellSnapshot.layer.shadowRadius = 5.0
+        cellSnapshot.layer.shadowOpacity = 0.4
+        return cellSnapshot
+    }
 
 }
 
@@ -403,6 +426,8 @@ extension BoardViewVC: UICollectionViewDataSource {
         return UICollectionViewCell()
         
     }
+    
+
     
 }
 
